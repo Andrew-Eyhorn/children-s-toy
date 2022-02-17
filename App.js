3a4844bada4350fd68c3f9bb24759bc9ce24a0d4
Andrew Eyhorn <andrew.eyhorn@gmail.com>
2022-02-17 19:45:51 +1100

    added more buttons

--- App.js
+++ App.js
@@ -5,21 +5,59 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>what do you mean by!</Text>
+      <Text>Match the Letter and Image!</Text>
       <StatusBar style="auto" />
       <Button
   title="A"
-  color="#000FFF"
+  color="#958890"
+/>
+<Button
+  title="B"
+  color="#958890"
+/>
+<Button
+  title="C"
+  color="#958890"
+/>
+<Button
+  title="D"
+  color="#958890"
+/>
+<Button
+  title="E"
+  color="#958890"
+/>
+<Button
+  title="Apple"
+  color="#958890"
+/>
+<Button
+  title="Banana"
+  color="#958890"
+/>
+<Button
+  title="Crocodile"
+  color="#958890"
+/>
+<Button
+  title="Dolphin"
+  color="#958890"
+/>
+<Button
+  title="Egg"
+  color="#958890"
 />
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    alignContent: 'left',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'left',
+    width:'10%',
+    height:'50%',
   },
 });
