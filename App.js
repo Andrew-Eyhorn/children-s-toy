619a6d2e742dd109636d710d45136a4950391753
Andrew Eyhorn <andrew.eyhorn@gmail.com>
2022-02-18 09:58:26 +1100

    added disabling of buttons that were not pressed, current issue is making a global variable

--- App.js
+++ App.js
@@ -1,63 +1,127 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import { render } from 'react-dom';
 import { StyleSheet, Text, View, Button } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Match the Letter and Image!</Text>
-      <StatusBar style="auto" />
-      <Button
+const LetterButtons = () => {
+  const [selectedLetter, letterSelected] = useState("none");
+ return(
+ <View style={styles.letterButtons}>
+  <Button  
   title="A"
   color="#958890"
+  onPress={() => {
+    letterSelected("A");
+  }}   
+  disabled={selectedLetter != "none" && selectedLetter != "A"}
 />
 <Button
   title="B"
   color="#958890"
+  onPress={() => {
+    letterSelected("B");
+  }}   
+  disabled={selectedLetter != "none" && selectedLetter != "B"}
 />
 <Button
   title="C"
   color="#958890"
+  onPress={() => {
+    letterSelected("C");
+  }}
+  disabled={selectedLetter != "none" && selectedLetter != "C"}   
 />
 <Button
   title="D"
   color="#958890"
+  onPress={() => {
+    letterSelected("D");
+  }}
+  disabled={selectedLetter != "none" && selectedLetter != "D"}   
 />
 <Button
   title="E"
   color="#958890"
+  onPress={() => {
+    letterSelected("E");
+  }}
+  disabled={selectedLetter != "none" && selectedLetter != "E"}   
 />
-<Button
-  title="Apple"
-  color="#958890"
-/>
-<Button
-  title="Banana"
-  color="#958890"
-/>
-<Button
-  title="Crocodile"
-  color="#958890"
-/>
-<Button
-  title="Dolphin"
-  color="#958890"
-/>
-<Button
-  title="Egg"
-  color="#958890"
-/>
+  </View>
+ )
+}
+const ImageButtons = () => {
+  const [selectedImage, imageSelected] = useState("none");
+  return(
+  <View style={styles.imageButtons}>
+ <Button
+   title="Apple"
+   color="#958890"
+   onPress={() => {
+    imageSelected("A");
+  }}   
+  disabled={selectedImage != "none" && selectedImage != "A"}
+ />
+ <Button
+   title="Banana"
+   color="#958890"
+   onPress={() => {
+    imageSelected("B");
+  }}   
+  disabled={selectedImage != "none" && selectedImage != "B"}
+ />
+ <Button
+   title="Crocodile"
+   color="#958890"
+   onPress={() => {
+    imageSelected("C");
+  }}   
+  disabled={selectedImage != "none" && selectedImage != "C"}
+ />
+ <Button
+   title="Dolphin"
+   color="#958890"
+   onPress={() => {
+    imageSelected("D");
+  }}   
+  disabled={selectedImage != "none" && selectedImage != "D"}
+ />
+ <Button
+   title="Egg"
+   color="#958890"
+   onPress={() => {
+    imageSelected("E");
+  }}   
+  disabled={selectedImage != "none" && selectedImage != "E"}
+ />
+   </View>
+  )
+ }
+ const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Match the Letter and Image!</Text>
+      <StatusBar style="auto" />
+      <LetterButtons />
+      <ImageButtons />
     </View>
-    
   );
 }
-
+export default App;
 const styles = StyleSheet.create({
   container: {
     alignContent: 'left',
     backgroundColor: '#fff',
     justifyContent: 'left',
+    display: 'flex',
+  },
+  letterButtons: {
     width:'10%',
     height:'50%',
+    display: 'flex'
+  },
+    imageButtons: {
+      width:'10%',
+      height:'50%',
+      display: 'flex',
   },
 });
