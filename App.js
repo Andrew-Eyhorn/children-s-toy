cdff76ca0f01bfe36af89fada227aa0184529097
Andrew Eyhorn <andrew.eyhorn@gmail.com>
2022-02-19 20:56:41 +1100

    almost fully complete, just need to fix component rendering and updating conflict

--- App.js
+++ App.js
@@ -2,110 +2,90 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { StyleSheet, Text, View, Button } from 'react-native';
-const LetterButtons = () => {
+const App = () => {
   const [selectedLetter, letterSelected] = useState("none");
- return(
- <View style={styles.letterButtons}>
-  <Button  
-  title="A"
-  color="#958890"
-  onPress={() => {
-    letterSelected("A");
-  }}   
-  disabled={selectedLetter != "none" && selectedLetter != "A"}
-/>
-<Button
-  title="B"
-  color="#958890"
-  onPress={() => {
-    letterSelected("B");
-  }}   
-  disabled={selectedLetter != "none" && selectedLetter != "B"}
-/>
-<Button
-  title="C"
-  color="#958890"
-  onPress={() => {
-    letterSelected("C");
-  }}
-  disabled={selectedLetter != "none" && selectedLetter != "C"}   
-/>
-<Button
-  title="D"
-  color="#958890"
-  onPress={() => {
-    letterSelected("D");
-  }}
-  disabled={selectedLetter != "none" && selectedLetter != "D"}   
-/>
-<Button
-  title="E"
-  color="#958890"
-  onPress={() => {
-    letterSelected("E");
-  }}
-  disabled={selectedLetter != "none" && selectedLetter != "E"}   
-/>
-  </View>
- )
-}
-const ImageButtons = () => {
+  const selectLetter = (letter) => {
+    letterSelected(letter)
+  }
   const [selectedImage, imageSelected] = useState("none");
-  return(
-  <View style={styles.imageButtons}>
- <Button
-   title="Apple"
-   color="#958890"
-   onPress={() => {
-    imageSelected("A");
-  }}   
-  disabled={selectedImage != "none" && selectedImage != "A"}
- />
- <Button
-   title="Banana"
-   color="#958890"
-   onPress={() => {
-    imageSelected("B");
-  }}   
-  disabled={selectedImage != "none" && selectedImage != "B"}
- />
- <Button
-   title="Crocodile"
-   color="#958890"
-   onPress={() => {
-    imageSelected("C");
-  }}   
-  disabled={selectedImage != "none" && selectedImage != "C"}
- />
- <Button
-   title="Dolphin"
-   color="#958890"
-   onPress={() => {
-    imageSelected("D");
-  }}   
-  disabled={selectedImage != "none" && selectedImage != "D"}
- />
- <Button
-   title="Egg"
-   color="#958890"
-   onPress={() => {
-    imageSelected("E");
-  }}   
-  disabled={selectedImage != "none" && selectedImage != "E"}
- />
-   </View>
-  )
- }
- const App = () => {
+  const selectImage = (image) => {
+    imageSelected(image)
+  }
+  const [selectedPair, pairSelected] = useState("none")
+  const selectPair = (type) => {
+    pairSelected(type)
+  }
   return (
     <View style={styles.container}>
       <Text>Match the Letter and Image!</Text>
       <StatusBar style="auto" />
-      <LetterButtons />
-      <ImageButtons />
+      <LetterButtons letter = {selectedLetter} selector={selectLetter} />
+      <ImageButtons image = {selectedImage} selector={selectImage} letter = {selectedLetter} choosePair = {selectPair}/>
+      <ResultDisplay chosenPair = {selectedPair} choosePair = {selectPair} selector={selectImage}/>
     </View>
   );
 }
+const letterButtonList = ["A", "B", "C", "D", "E"]
+const LetterButtons = (props) => {
+ return(
+ <View style={styles.letterButtons}>
+   {letterButtonList.map((name, index) => 
+    <Button  key = {index}
+  title= {name}
+  color="#958890"
+  onPress={() => {
+    props.selector(name);
+  }}   
+  disabled={props.letter != "none" && props.letter != name}/>
+)}
+  </View>
+ )
+}
+const imageButtonList = ["Apple", "Banana", "Crocodile", "Dragoon", "Egg"]
+const ImageButtons = (props) => {
+  return(
+  <View style={styles.imageButtons}>
+{imageButtonList.map((name, index) => 
+    <Button  key = {index}
+  title= {name}
+  color="#958890"
+  onPress={() => {
+    props.selector(name);
+    if(name.charAt(0) === props.letter) {
+      props.choosePair("Correct")
+    } else {
+      props.choosePair("Incorrect")
+    }
+  }}   
+  disabled={props.image != "none" && props.image != name}/>
+)}
+   </View>
+  )
+ }
+ const ResultDisplay = (props) => {
+  if (props.chosenPair === "none") {
+    return(<View><Text></Text></View>)
+} else if (props.chosenPair === "correct"){
+  return(<View><Text>{props.chosenPair}</Text></View>)
+}else {
+  props.choosePair("none")
+  props.selector("none")
+  return(<View><Text>{props.chosenPair}</Text></View>)
+}
+}
+// const GenerateResultDisplay = (props) => {
+//   if (props.chosenPair === "none") {
+//     return("")
+// } else if (props.chosenPair === "correct"){
+//   return(props.chosenPair)
+// }else {
+//   props.choosePair("none")
+//   props.selector("none")
+//   return(props.chosenPair)
+// }}
+// const ResultDisplay = (props) => {
+//   return(<View><Text>{GenerateResultDisplay(props)}</Text></View>)
+// }
 export default App;
 const styles = StyleSheet.create({
   container: {
@@ -115,12 +95,12 @@ const styles = StyleSheet.create({
     display: 'flex',
   },
   letterButtons: {
-    width:'10%',
+    width:'20%',
     height:'50%',
     display: 'flex'
   },
     imageButtons: {
-      width:'10%',
+      width:'20%',
       height:'50%',
       display: 'flex',
   },
